35d86f07123ca549284f9304b065d85b0268b1fe
Yashveer singh <yashveersingh30401@gmail.com>
2024-05-09 22:40:50 +0530

    Dep added

--- app/src/main/java/com/yash/notebuddy/MainActivity.kt
+++ app/src/main/java/com/yash/notebuddy/MainActivity.kt
@@ -20,28 +20,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             NotebuddyTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NotebuddyTheme {
-        Greeting("Android")
-    }
-}
