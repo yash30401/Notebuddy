99486b8e1e11e7ed68dca36ff757f30d27fd3240
Yashveer singh <yashveersingh30401@gmail.com>
2024-06-03 15:41:03 +0530

    Frist TestCase

--- app/src/test/java/com/yash/notebuddy/feature_note/domain/use_case/GetNotesUseCaseTest.kt
+++ app/src/test/java/com/yash/notebuddy/feature_note/domain/use_case/GetNotesUseCaseTest.kt
@@ -1,12 +1,12 @@
 package com.yash.notebuddy.feature_note.domain.use_case
 
+import com.google.common.truth.Truth.assertThat
 import com.yash.notebuddy.feature_note.data.repository.FakeNoteRepository
 import com.yash.notebuddy.feature_note.domain.model.Note
 import com.yash.notebuddy.feature_note.domain.util.NoteOrder
 import com.yash.notebuddy.feature_note.domain.util.OrderType
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.runBlocking
-import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Test
 
@@ -41,6 +41,9 @@ class GetNotesUseCaseTest {
     fun `Order notes by title ascending, correct order`() = runBlocking{
         val notes = getNotesUseCase(NoteOrder.Title(OrderType.Ascending)).first()
 
+        for(i in 0..notes.size-2){
+            assertThat(notes[i].title).isLessThan(notes[i+1].title)
+        }
     }
 
 }
